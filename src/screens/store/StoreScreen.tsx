03cff55634b71e6e9d456bad7ac1d649e70f6380
Artur Jankowski <arturjankowski95+1@gmail.com>
2026-06-09 19:49:55 +0200

    update

--- src/screens/store/StoreScreen.tsx
+++ src/screens/store/StoreScreen.tsx
@@ -14,6 +14,7 @@ import Button from '../../components/molecules/Button';
 import IconButton from '../../components/molecules/IconButton';
 import { useResponsive } from '../../responsive/useResponsive';
 import { useTheme } from '../../theme';
+import { getContrastColor } from '../../theme/colorUtils';
 import { useTranslation } from '../../i18n/TranslationContext';
 import { STORE_CATEGORIES, STORE_ICONS, type StoreCategory } from '../../data/store';
 import type { CatalogEntry, PackDefinition } from '../../data/store/types';
@@ -23,15 +24,6 @@ import type { RootStackParamList } from '../../navigation/types';
 import { ThemePreview } from './ThemePreview';
 
 type StoreScreenProps = NativeStackScreenProps<RootStackParamList, 'Store'>;
-
-function getContrastColor(hexColor: string): string {
-    const color = hexColor.replace('#', '');
-    const r = parseInt(color.substring(0, 2), 16);
-    const g = parseInt(color.substring(2, 4), 16);
-    const b = parseInt(color.substring(4, 6), 16);
-    const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
-    return luminance > 0.6 ? '#000000' : '#FFFFFF';
-}
 
 function StoreItemCard({
     entry,
